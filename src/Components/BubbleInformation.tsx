bcc2bb896e42fa6fbbf59891f1874c3a5fb37018
Junaid Qureshi <junaid.qureshi1995@gmail.com>
2021-08-15 18:49:31 -0500

    Added bubble component
    Added bubble component

--- src/Components/BubbleInformation.tsx
+++ src/Components/BubbleInformation.tsx
@@ -1,5 +1,39 @@
-const BubbleInformation = () => {
-    return <div></div>;
+import { Box, createStyles, Icon, makeStyles, Theme, Typography } from "@material-ui/core";
+
+const bubbleStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        icon: {
+            width: "10rem",
+            height: "10rem",
+        },
+        text: {
+            marginLeft: "2rem",
+            marginTop: "4rem",
+        },
+    })
+);
+
+interface props {
+    iconPath: string;
+    description: string;
+}
+
+const BubbleInformation = (props: props) => {
+    const styles = bubbleStyles();
+    return (
+        <Box display="flex" justifyContent="center">
+            <Box component="span">
+                <Icon>
+                    <img className={styles.icon} src={props.iconPath} alt="Bubble Icon" />
+                </Icon>
+            </Box>
+            <Box component="span">
+                <Typography className={styles.text} variant="body1">
+                    {props.description}
+                </Typography>
+            </Box>
+        </Box>
+    );
 };
 
 export default BubbleInformation;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,8 @@
 import { Box, createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
+import BubbleInformation from "../Components/BubbleInformation";
+import img1 from "../Images/dvd.svg";
+import img2 from "../Images/streaming.svg";
+import img3 from "../Images/information.svg";
 
 const homeStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -20,6 +24,16 @@ const Home = () => {
                 Welcome to the number one movie viewing/rental site
             </Typography>
             <hr className={styles.hrStyle} />
+            <BubbleInformation
+                iconPath={img1}
+                description="Get your movies delivered directly to you"
+            />
+            <BubbleInformation iconPath={img2} description="Stream movies at your leisure" />
+            <BubbleInformation
+                iconPath={img3}
+                // eslint-disable-next-line max-len
+                description="Once you're done watching remember to write a review (Feature In Development)"
+            />
         </Box>
     );
 };
